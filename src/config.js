ce95cb740157f83303e469af9a323f2287090e80
unknown <1152537785@qq.com>
2020-05-02 13:53:51 +0800

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+
+export default {
+   net: {
+       1: { //数字代表不同的以太坊网络(networkVersion) 1是主网络
+           contractAddress : "0x066C87c2B1A685FdBFBC840554F0f72020295b08",
+       },
+       1337: {
+           contractAddress : "0xB28De01Bd3299c8956CBb32002a137F32FfACB15",
+       }
+   }
+}
